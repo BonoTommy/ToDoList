15faed7da39c333e955fc8d94e02b78562b485ba
Tommy-Bonneau <bonneautommy00@gmail.com>
2022-10-24 21:16:47 -0400

    Changement dans l'accordion

--- accordion.js
+++ accordion.js
@@ -2,23 +2,20 @@ class Accordion {
     constructor() {
         this._elDivAccordion = document.querySelector('[data-js-detail]');
 
-        this._elImgChevron = document.querySelector('img');
+        this._elSpanChevron = document.querySelector('.chevron');
 
         this.init();
     }
 
     init() {
-        this._elImgChevron.addEventListener('click',this.fermeTiroir.bind(this));
+        this._elSpanChevron.addEventListener('click',this.actionTiroir.bind(this));
     }
 
-    fermeTiroir() {
+    actionTiroir() {
         
         this._elDivAccordion.classList.toggle('is-open');
-        
-        if(this._elImgChevron.src != 'down.svg') {
-            this._elImgChevron.src = 'down.svg';
-        } else if (this._elImgChevron.src != 'up.svg') {
-            this._elImgChevron.src = 'up.svg'
-        }
+
+        this._elSpanChevron.classList.toggle('up');
+        this._elSpanChevron.classList.toggle('bottom');
     }
 }
--- organiserTaches.js
+++ organiserTaches.js
@@ -6,6 +6,9 @@ class OrganiserTaches {
         this._elBtnTriNum = document.querySelector('[data-js-trinum]');
         //this._elsBtnTri = document.querySelectorAll('[data-js-action]')
         
+        this._elTaches = document.querySelector('[data-js-sectionListe]');
+
+        console.log(this._elTaches)
 
         //this._elParent = this._el.closest('[data-js-liste]');
         
@@ -13,14 +16,54 @@ class OrganiserTaches {
     }
 
     init() {
-        this._elBtnTriAlpha.addEventListener('click', this.triTache.bind(this));
-        this._elBtnTriNum.addEventListener('click', this.triNum.bind(this));
+      
 
-        /*this._elSectionListe.addEventListener('click', function (e) {
-            if (e.target.dataset.jsAction == 'tache') this.triTache();
-            if (e.target.dataset.jsAction == 'importance') this.triNum();
-        }).blind(this)*/
+
+        this._elTaches.addEventListener('click', function(e) {
+            let cle = e.target.dataset.jsTri;
+            if (cle == 'tache') this.tri(cle);
+            if (cle == 'importance') this.tri(cle);
+        }.bind(this))
     }
+
+
+
+
+
+    tri(cle) {
+       
+        console.log(cle)
+
+        toDoList.sort(function(a, b) {
+                
+            if (a[cle] > b[cle]) {
+                return 1;
+            }
+            if (b[cle] > a[cle]) {
+                return -1;
+            }
+            return 0; 
+                
+        })
+
+        this._elParent.innerHTML = '';
+
+        for (let i = 0, l = toDoList.length; i < l; i++) {
+            let elListe = document.querySelector('[data-js-liste]');
+
+            let elNouvelDivListe = `<div class="wrapper-flex-row" data-js-tache="${i}">
+                                    <p><small>Tâche : </small>${toDoList[i].tache} - <small>Importance : </small>${toDoList[i].importance}</p>
+                                    <button class="btn-liste" type="submit" data-js-btnAfficheDetail="${i}">Afficher le détail</button>
+                                    <button class="btn-liste" type="submit" data-js-btnSupprimer="${i}">Supprimer</button>
+                                </div>`
+
+            elListe.insertAdjacentHTML('beforeEnd', elNouvelDivListe);
+            new Tache(elListe.lastElementChild);
+        }
+    }
+
+
+
 
 
     triTache() {
